82757fbc9c620c54b389db99e8478a941c91fc39
nagatoroimo <nagatoroimo@gmail.com>
2018-08-09 08:41:55 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Message.create(title: "テストタイトル１", content: "テストコンテンツ１")
+Message.create(title: "テストタイトル２", content: "テストコンテンツ２")
+Message.create(title: "テストタイトル３", content: "テストコンテンツ３")
